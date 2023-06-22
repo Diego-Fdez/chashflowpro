0e64da0a33b2197699a30ec5be875b173816fdec
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-22 12:07:08 -0600

    add auth route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .users.routes import users_routes
+from app.auth.routes import auth_routes
 
 
 # Create an instance of the FastAPI class
@@ -13,6 +14,7 @@ app = FastAPI(
 
 # instantiate the router object
 app.include_router(users_routes.router)
+app.include_router(auth_routes.router)
 
 
 # Define the root route
