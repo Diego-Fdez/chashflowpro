b5347040727c390ce1541534243ac6acdfd3498a
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-22 18:43:04 -0600

    add products routes

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .users.routes import users_routes
 from app.auth.routes import auth_routes
 from app.categories.routes import categories_routes
 from app.clients.routes import clients_routes
+from app.products.routes import products_routes
 
 
 # Create an instance of the FastAPI class
@@ -19,6 +20,7 @@ app.include_router(users_routes.router)
 app.include_router(auth_routes.router)
 app.include_router(categories_routes.router)
 app.include_router(clients_routes.router)
+app.include_router(products_routes.router)
 
 
 # Define the root route
